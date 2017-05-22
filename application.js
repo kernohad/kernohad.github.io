fa720068b1556d3f617db8ace179b90128ded4ec
Dylan Kernohan <kernohad@mail.gvsu.edu>
2017-05-22 19:39:01 -0400

    Added modal

--- application.js
+++ application.js
@@ -1,3 +1,37 @@
+var model = {
+	GVSU: {
+		title: "GVSU", 
+		description:"I am going to GVSU",
+		imageSrc: ""
+	},
+	LCC: {
+		title: "LCC", 
+		description: "I went to LCC",
+		imageSrc: ""
+	},
+	OE: {
+		title: "O-E", 
+		description:"I went to O-E",
+		imageSrc: ""
+	},
+	KLA: {
+		title: "KLA", 
+		description:"I am interning at KL&A",
+		imageSrc: ""
+	},
+	GPR: {
+		title: "Genius Phone Repair", 
+		description:"I used to work at Genius Phone Repair",
+		imageSrc: ""
+	},
+	LS: {
+		title: "GVSU Laker Store", 
+		description:"I used to work at the GVSU Laker Store",
+		imageSrc: ""
+	}
+}
+
+
 $(document).ready(function() {
 
 	$("#button-box").on('mouseenter' , function() {
@@ -25,5 +59,15 @@ $(document).ready(function() {
 	 	$(this).parent().css('border', 'solid 1px black');
 
 	 });
+
+
+	$(".thumbnail").on("click", function() {
+		var modalTitle = $(this).find("h3").text();
+		var data = $(this).data("model");
+		console.log(data);
+		$(".modal-title").text(model[data].title);
+		$(".modal-body").text(model[data].description);
+
+	});
 
 });
